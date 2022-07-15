51cf67efd20de38c6a32c63dfd99a72365eb9f81
Mario Strachan <96462826+Rio242@users.noreply.github.com>
2022-07-15 02:03:12 +0000

    Added randomizer function to each for loop instance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Password Generator Project
-import random
+import random # to get random function
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -13,21 +13,24 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 for letter in range(0,nr_letters):
-  
+  l = random.sample(letters,nr_letters)
+
 
 
 for number in range(0,nr_numbers):
+  n = random.sample(numbers,nr_numbers)
+
+
+
+for symbol in range(0,nr_symbols):
+  s = random.sample(symbols,nr_symbols)
 
 
 
 
-for symbols in range(0,nr_symbols):
+password = [l,n,s]
 
-
-
-password = []
-
-print(''.join(password))
+print(password)
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
